5bebe242f7e9f9d82aa6a1c476c4d9b676ac20ae
alumno <alumno@10.0.2.15>
2017-04-03 13:21:33 +0200

    Subir archivos a github

--- ED/src/ed/ED.java
+++ ED/src/ed/ED.java
@@ -15,7 +15,7 @@ public class ED {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        public int prueba;
     }
     
 }
